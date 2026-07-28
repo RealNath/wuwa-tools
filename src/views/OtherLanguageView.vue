50caa539e38766abbb716eb21dbf698a9b9f9a75
RealNath <87565567+RealNath@users.noreply.github.com>
2026-07-28 17:06:47 +0700

    feat: search based on a language (WIP)

--- src/views/OtherLanguageView.vue
+++ src/views/OtherLanguageView.vue
@@ -1,9 +1,39 @@
 <script setup lang="ts">
 import { onMounted, onUnmounted, ref } from 'vue'
 import { Input } from '@/components/ui/input'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuRadioGroup,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
 import DataWorker from '../workers/dataWorker?worker'
+import { Button } from '@/components/ui/button'
+
+const languages = ref<string[]>([
+  'de',
+  'en',
+  'es',
+  'fr',
+  'ja',
+  'ko',
+  'pt',
+  'th',
+  'zh-Hans',
+  'zh-Hant',
+])
 
 const searchQuery = ref<string>('')
+const position = ref<string>('en')
 
 const items = ref<any[]>([])
 const errorMessage = ref<string>('')
@@ -26,7 +56,7 @@ function getOtherLanguageById(event: Event, item: any) {
   if (detailsElement.open && item.wikiText === 'Loading data...') {
     worker?.postMessage({
       command: 'get_other_language_by_id',
-      id: item.questId,
+      questId: item.questId,
     })
   }
 }
@@ -64,15 +94,38 @@ onUnmounted(() => {
 <template>
   <div class="other-language">
     <h1 class="text-3xl font-bold">Other Language</h1>
-    <Input
-      id="other-language-search"
-      :disabled="isLoading"
-      v-model="searchQuery"
-      @keydown.enter="handleSearch"
-      type="text"
-      placeholder="Search for the exact full string here."
-    >
-    </Input>
+    <div class="flex items-center gap-2">
+      <Input
+        id="other-language-search"
+        :disabled="isLoading"
+        v-model="searchQuery"
+        @keydown.enter="handleSearch"
+        type="text"
+        placeholder="Search for the exact full string here."
+      >
+      </Input>
+      <DropdownMenu>
+        <!--  -->
+        <DropdownMenuTrigger as-child>
+          <Button variant="outline">{{ position }}</Button>
+        </DropdownMenuTrigger>
+
+        <DropdownMenuContent>
+          <!--  -->
+          <DropdownMenuGroup>
+            <DropdownMenuLabel>Select a Language (WIP)</DropdownMenuLabel>
+
+            <DropdownMenuRadioGroup v-model="position">
+              <DropdownMenuRadioItem v-for="lang in languages" :key="lang" :value="lang">
+                {{ lang }}
+              </DropdownMenuRadioItem>
+            </DropdownMenuRadioGroup>
+          </DropdownMenuGroup>
+          <!--  -->
+        </DropdownMenuContent>
+        <!--  -->
+      </DropdownMenu>
+    </div>
 
     <div v-if="isLoading">Loading data...</div>
     <div v-else-if="errorMessage">Error: {{ errorMessage }}</div>
